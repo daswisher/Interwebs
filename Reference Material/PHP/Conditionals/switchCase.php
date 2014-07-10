0bffbccc8bca9e5b47b63b285a536aeb3c849c08
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-07-09 23:32:19 -0600

    Reorganizing repo to separate projects and reference material (which is splite based upon language) as well as added some material on php

---
+++ Reference Material/PHP/Conditionals/switchCase.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+        switch($val){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    ?>
+	</body>
+</html>
