f9242bb299a630e6f17f27419bdeb99b045b7754
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-07-17 23:59:34 -0600

    Added a section on loops

---
+++ Reference Material/PHP/Loops/forLoops.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head><title></title></head>
+	<body>
+		<p>
+			<?php
+				for ($i = 0; $i <= 100; $i = $i + 10){
+					echo $i.'<br />';
+				}
+			?>
+		</p>
+	</body>
+</html>
---
+++ Reference Material/PHP/Loops/whileLoops.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head><title></title></head>
+	<body>
+		
+			<?php
+				$cond = true;
+				do{
+					echo "<p>This is a loop.</p>";
+					$cond = false;
+				}while($cond);
+		      	?>
+	</body>
+</html>
