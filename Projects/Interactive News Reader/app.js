1203dc43d3b7dac1014f49de22c7d5a20391c8c0
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-08-01 07:25:02 -0600

    Adding the bootstrap, jQuery, and background images to the interactive news reader directory. Finished the jQuery code that makes the news reader functional in terms of opening articles as well as hotkey functions

--- Projects/Interactive News Reader/app.js
+++ Projects/Interactive News Reader/app.js
@@ -6,6 +6,15 @@ var main = function () {
         $(this).children('.description').show();
     });
     $(document).keypress(function (event) {
+        if (event.which == 111) {
+            $('.current').children('.description').toggle();
+        }
+        else if (event.which == 110) {
+            var currentArticle = $('.current');
+            var nextArticle = currentArticle.next();
+            currentArticle.removeClass('current');
+            nextArticle.addClass('current');
+        }
     });
 };
 
